00f98269db0c6b0ecbd6f05b5cfe8ce67e1420fb
Sriram-selvaraj-api <sriramsearch44@gmail.com>
2025-04-20 13:51:58 +0530

    added store link

--- src/components/Promotion.tsx
+++ src/components/Promotion.tsx
@@ -20,8 +20,8 @@ const DownloadSection = () => (
           <Grid className={Styles.contentImageGrid} size={{ xs: 12, md: 5, lg: 6 }}>
             <p className={Styles.boostContent}>{Constant.boostYourProduction}</p>
             <div className={Styles.storeImageDiv}>
-              <img alt='play-store' src={IMAGES.PlayStore} width={146} />
-              <img alt='app-store' className={Styles.appStore} src={IMAGES.AppStore} width={132} />
+              <a href="https://play.google.com/store/search?q=bhive+workspace&c=apps&hl=en_IN" rel="noreferrer" target="_blank"> <img alt='play-store' src={IMAGES.PlayStore} width={146} /> </a>
+              <a href="https://apps.apple.com/in/app/bhive-workspace/id6463923684" rel="noreferrer" target="_blank"><img alt='app-store' className={Styles.appStore} src={IMAGES.AppStore} width={132} /></a>
             </div>
           </Grid>
         </Grid>
